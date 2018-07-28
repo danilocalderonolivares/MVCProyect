ff203e26fbb10a56c328df17b5893b3661bda8fe
danilocalderonolivares <dcalderono@ucenfotec.ac.cr>
2018-07-27 22:21:57 -0600

    First commit
    Add and test the first view(Movie view)

---
+++ MVCProyect/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCProyect.Models.POCOS;
+
+namespace MVCProyect.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Test
+        public ActionResult Test()
+        {
+
+            var movie = new Movie 
+            {
+                Name="Test"
+            };
+            return View(movie);
+        }
+    }
+}
---
+++ MVCProyect/Models/POCOS/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCProyect.Models.POCOS
+{
+    public class Movie
+    {
+        public int MoviId { get; set; }
+        public string Name { get; set; }
+
+        public Movie()
+        {
+
+        }
+    }
+}
